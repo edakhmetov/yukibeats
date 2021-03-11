f554f0989a44133ee22e3faf36e78a650ac71c84
edakhmetov <78399160+edakhmetov@users.noreply.github.com>
2021-03-11 18:21:40 -0500

    removed body parser; cleaned up sass; added sass variables

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is my first personal project as a web developer. 
 The website is a showcase of my friend's rap beats that he's been producing over the last 5 years.
 The website is still in the development phase with changes happening every day.
-Front-end: HTML, CSS, JS, 
+Front-end: HTML, Sass, JS, 
 Back-end: NodeJS, MongoDB, Express
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv/config');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
@@ -10,8 +9,8 @@ const indexRoute = require('./routes/index');
 const contactRoute = require('./routes/contact');
 
 // middleware 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
